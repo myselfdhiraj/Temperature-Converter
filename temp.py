29a04c6c2b68fbcb74b7108b5920feec38c2fe11
Dhiraj Arya <146155093+myselfdhiraj@users.noreply.github.com>
2023-10-08 07:01:24 +0530

    Create temp.py

---
+++ temp.py
@@ -0,0 +1,7 @@
+temp = int(input("Enter Temperature[Degree calsius]: "))
+
+# formula = (0C*9/5)+32 = F
+ftemp = (temp*9/5)+32
+
+print("Temperature in Fahrenheit: ",ftemp)
+print("\n Thanks to test my program")
